4641760464f1dd38a71f664dc7f031356c9df506
hnguyen <hnguyen@exacttarget.com>
2015-05-18 17:54:14 -0400

    jared changes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var sass = require('node-sass');
+var fs = require('fs');
+
+var inputPath = './';
+var outputFilePath = './';
+var inputFileName = 'test';
+var inputFileExt = '.scss';
+var outputStyle = "compact";
 
 sass.render({
-	file: './test.scss',
+	file: inputPath + inputFileName + inputFileExt,
+	outputStyle: outputStyle
 }, function (error, result) {
-	console.log('result: \n' + error + '\n ======= \n' + result.css);
+	if (error) throw error;
+	fs.writeFile(outputFilePath + inputFileName + '.css', result.css, function (err) {
+		if (err) throw err;
+		console.log('saved');
+	})
 });
